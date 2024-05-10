5c002b84cb93d8b7b17afc1586942ed9bdfed706
froilaaaaan1 <froilanaquino1@gmail.com>
2024-05-11 00:45:07 +0800

    init other pages and added insights and analytics

---
+++ app/admin/nurses/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NursesPage = () => {
+	return <div>NursesPage</div>;
+};
+
+export default NursesPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'react-hot-toast';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -27,6 +29,8 @@ export default function RootLayout({
 					<main className='min-h-screen w-full'>{children}</main>
 				</body>
 			</TooltipProvider>
+			<Analytics />
+			<SpeedInsights />
 		</html>
 	);
 }
